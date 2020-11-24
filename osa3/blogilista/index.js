8880a50ee8c779257884b692364101f25a7df4fb
Samu <samu.koivu1@hotmail.com>
2020-11-24 16:27:16 +0200

    Schema omassa moduulissa

--- osa3/blogilista/index.js
+++ osa3/blogilista/index.js
@@ -3,23 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = process.env.MONGOURI
 
